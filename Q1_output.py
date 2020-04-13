8f5ebdfc71198cbe6b8f392c7d825cb905dabcbd
YarinBS <yarin10121@gmail.com>
2020-04-13 17:28:50 +0300

    Q1_output

---
+++ Q1_output.py
@@ -0,0 +1,9 @@
+population_type = ['Men', 'Women', 'All']
+
+print("Question 1:\n")
+for population in population_type:
+    print(f"{population}:\n"
+          f"Age: {}, {}, {}\n"
+          f"Earnings: {}, {}, {}\n"
+          f"Hours: {}, {}, {}\n"
+          f"Week: {}, {}, {}")
