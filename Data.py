c0af3e29ed1d2a11e94cc4fb316dbd33c7804038
Roi <thedarkhermit@gmail.com>
2020-04-14 11:26:44 +0300

    starttt

--- Data.py
+++ Data.py
@@ -3,6 +3,11 @@ import math
 
 
 def load_data(path, features):
+    """
+    :param path: path to the file
+    :param features: list of features
+    :return: data after normalisation and choosing the interesting features
+    """
     df = pd.read_csv(path)
     data = df.to_dict(orient="list")
     filtered_data = {k: data[k] for k in features}  # construct dict only with the features we have
@@ -10,12 +15,18 @@ def load_data(path, features):
     return filtered_data
 
 
-def filter_by_features(data, feature, vlaues):
+def filter_by_features(data, feature, values):
+    """
+    :type values: set
+    :type feature: list
+    :type data: dict
+    :return : 2 dicts one with the fitting condition and one without
+    """
     list_of_wanted_indexes = []
     dict_of_fitting_values = {}
     l1 = []
     for index, elem in enumerate(data[feature]):
-        if elem in vlaues:
+        if elem in values:
             list_of_wanted_indexes.append(index)
 
     for key, value in data.items():
